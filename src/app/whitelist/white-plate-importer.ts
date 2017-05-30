214ebb8d53b77e7f134d1e689fb37a401fb18dcd
Matas Seimys <matas.seimys@gmail.com>
2017-05-30 23:54:32 +0300

    Whitelist file uploader now uses the same http provider as the rest of the app

--- src/app/whitelist/white-plate-importer.ts
+++ src/app/whitelist/white-plate-importer.ts
@@ -1,18 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'white-plate-importer',
   template: `
-    <div>
+    <div class="pull-right importer">
       <input type="file" (change)="fileChangeEvent($event)" placeholder="Import file..." />
       <button (click)="importWhitelist()" class="btn btn-primary">Import whitelist</button>
+      <p *ngIf="message" class="text-success">{{message}}</p>
+      <p *ngIf="error" class="text-danger">{{error}}</p>
     </div>
-  `
+  `,
+  styles: [`
+    .importer {
+      padding-bottom: 10px;
+      text-align: right;
+    }
+    input {
+      display: inline-block;
+    }
+  `]
 })
 export class WhitePlateImporterComponent {
   @Input() onPlateImported: Function;
   filesToUpload: File[];
+  message: string;
+  error: string;
 
   constructor(public http: Http) {
     this.filesToUpload = [];
@@ -24,31 +38,23 @@ export class WhitePlateImporterComponent {
     this.filesToUpload = <File[]> fileInput.target.files;
   }
   importWhitelist() {
-    this.makeFileRequest('/api/whitelist/upload', [], this.filesToUpload).then((result) => {
-      this.onPlateImported();
-    }, (error) => {
-        console.error(error);
-    });
+    this.message = this.error = '';
+    this.makeFileRequest('/api/whitelist/upload', [], this.filesToUpload).subscribe(
+      result => {
+        this.message = `Total plate numbers: ${result.total}. Imported: ${result.imported}. New: ${result.new}`;
+        this.onPlateImported();
+      },
+      err => {
+        this.error = 'Import failed: ' + err;
+        return Observable.throw(err);
+      }
+    );
   }
   makeFileRequest(url: string, params: string[], files: File[]) {
-    return new Promise((resolve, reject) => {
-        let formData: any = new FormData();
-        let xhr = new XMLHttpRequest();
-        for (let file of files) {
-            formData.append('uploads', file, file.name);
-        }
-        let requestfinished = 4;
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === requestfinished) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.response));
-                } else {
-                    reject(xhr.response);
-                }
-            }
-        };
-        xhr.open('POST', url, true);
-        xhr.send(formData);
-    });
+    let formData: any = new FormData();
+    for (let file of files) {
+        formData.append('uploads', file, file.name);
+    }
+    return this.http.post(url, formData).map(res => res.json());
   }
 }
